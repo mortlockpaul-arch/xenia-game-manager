6fb264b6b3295beb7f2e697fa98ca843efd46230
PAUL MORTLOCK <mortlock.paul@gmail.com>
2026-06-20 19:08:06 +0100

    0.5.9

--- src/config.py
+++ src/config.py
@@ -1,26 +1,34 @@
 import json
 import os
+import sys
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-CONFIG_DIR = os.path.join(BASE_DIR, "config")
-
-
-CONFIG_FILE = os.path.join(CONFIG_DIR, ".x360-game-manager-config.json")
+def get_app_dir():
+    if getattr(sys, "frozen", False):
+        return os.path.dirname(sys.executable)  # ✔ correct for frozen apps
+    return os.path.dirname(os.path.abspath(__file__))
 
 def load_config():
-    if not os.path.exists(CONFIG_FILE):
-        return {}
-
+    datadir = get_app_dir()
+    config_dir = os.path.join(datadir, "config")
+    config_file = os.path.join(config_dir, ".x360-game-manager-config.json")
+    print(config_dir)
+    print(config_file)
     try:
-        with open(CONFIG_FILE, "r", encoding="utf-8") as f:
+        with open(config_file, "r", encoding="utf-8") as f:
             return json.load(f)
-    except Exception:
+    except Exception as e:
+        print("Config load error:", e)
         return {}
 
 
 def save_config(data: dict):
+    datadir = get_app_dir()
+    config_dir = os.path.join(datadir, "config")
+    config_file = os.path.join(config_dir, ".x360-game-manager-config.json")
+    print(config_dir)
+    print(config_file)
     try:
-        with open(CONFIG_FILE, "w", encoding="utf-8") as f:
+        with open(config_file, "w", encoding="utf-8") as f:
             json.dump(data, f, indent=4)
     except Exception as e:
         print("Config save error:", e)
