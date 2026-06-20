f4ffdbbd3bf175c87e0efa52bc849ea7950703af
PAUL MORTLOCK <mortlock.paul@gmail.com>
2026-06-20 16:11:24 +0100

    0.5.4

--- build_tools/setup.py
+++ build_tools/setup.py
@@ -23,7 +23,7 @@ build_exe_options = {
     "excludes": ["tkinter", "unittest"],
     "include_files": [
         (str(ROOT / "src" / "db"), "db"),
-        (str(ROOT / "assets"), "assets"),
+        (str(ROOT / "src" / "config"), "config"),
     ],
     "optimize": 2,
 }
@@ -35,6 +35,7 @@ bdist_msi_options = {
     "initial_target_dir": r"[ProgramFilesFolder]\%s" % APP_NAME,
     "output_name": "XeniaGameManager-win64.msi",
     "product_name": "Xenia Game Manager",
+    "product_version": VERSION,
     "data": {
         "Icon": [
             ("IconId", str(ROOT / "assets" / "icons" / "app.ico")),
@@ -46,6 +47,7 @@ setup(
     name=APP_NAME,
     version=VERSION,
     description=APP_NAME,
+    author="Xenia Game Manager",
     options={
         "build_exe": build_exe_options,
         "bdist_msi": bdist_msi_options,
