1726779b071e600aef94f553c792cd33d16fcecc
PAUL MORTLOCK <mortlock.paul@gmail.com>
2026-07-03 17:41:42 +0100

    0.7.3

--- build_tools/build.py
+++ build_tools/build.py
@@ -1,12 +1,21 @@
 import subprocess
 import sys
 
+
 def run():
+    # Build
     subprocess.check_call([
         sys.executable,
         "build_tools/setup.py",
         "build"
     ])
+
+    # Package
+    subprocess.check_call([
+        sys.executable,
+        "build_tools/package.py"
+    ])
+
 
 if __name__ == "__main__":
     run()
